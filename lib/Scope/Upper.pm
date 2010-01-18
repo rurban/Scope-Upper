34257ad32ccc3d743ea1c90fa6cc051c7821e82f
Vincent Pit <vince@profvince.com>
2010-01-18 22:51:22 +0100

    Bump copyright year

--- lib/Scope/Upper.pm
+++ lib/Scope/Upper.pm
@@ -404,7 +404,7 @@ Thanks to Shawn M. Moore for motivation.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008-2009 Vincent Pit, all rights reserved.
+Copyright 2008,2009,2010 Vincent Pit, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
